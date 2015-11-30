253d2cf24913e82a20bcb944cf530a01432042c2
Joe Marchese <ZoogerZager@users.noreply.github.com>
2015-11-30 16:08:56 -0500

    Finally fixed the IndexError Handling
    Should run until completion now. Want to add a subfolder for the result.txts next.

--- PolyNana.py
+++ PolyNana.py
@@ -1,10 +1,10 @@
 """
 
-A program designed to randomize a pollyanna gift exchange where parents
-cannot get gifts for their children and children cannot get gifts
-for their parents. Neither can spouses.
-
-with Nanna.
+A program designed to randomize a pollyanna gift exchange where
+- Parents cannot give to their children.
+- Children cannot give to their parents.
+- Siblings cannot give to each other.
+- Spouses/Couples cannot give to each other.
 
 """
 
@@ -15,21 +15,20 @@ class Person:
     def __init__(self, name, restricted_list=None, giving_to=None):
         self.name = name
         self.restricted_list = restricted_list
+        self.giving_to = giving_to
         
-    def select(self):
-        okay_list = []
-        for person in part_strings:
-            okay_list.append(person)
-        restricted_list = self.restricted_list
-        for name in restricted_list:
-            if name in okay_list:
-                okay_list.remove(name)
-            else:
-                pass
-        self.giving_to = random.choice(okay_list)
-        part_strings.remove(self.giving_to)
-            
-            
+def select(person, hat_list):
+    '''person should be a Person object. hat_list should be a list of strings of participants.'''
+    okay_list = []
+    for each in hat:
+        okay_list.append(each)
+    for name in person.restricted_list:
+        if name in okay_list:
+            okay_list.remove(name)
+        else:
+            pass        
+    return random.choice(okay_list)
+    
 joe = Person('Joe', ['Joe', 'Jeff', 'Dave', 'Adam'])
 dave = Person('Dave', ['Dave', 'Jeff', 'Adam', 'Joe'])
 adam = Person('Adam', ['Adam', 'Jeff', 'Dave', 'Joe'])
@@ -46,28 +45,27 @@ nanna = Person('Nanna', ['Jeff', 'Renee', 'Angela'])
 participants = [joe, dave, adam, justin, stefan, amanda, francesca, jeff,
 angela, renee, george, nanna]
 
-part_strings = []
-for participant in participants:
-    part_strings.append(participant.name)
-
-while part_strings:
-    try:
-        for participant in participants:
-            participant.select()
-        # Optional Code to print out to Console rather than .txt
-        # for participant in participants:
-            # print(participant.name, 'gives to', participant.giving_to)
-        print('Success. You are awesome.')
-    except IndexError:
-        print('You may be awesome but it did not work. Running the program again.')
-        quit()
-        
-with open('full_results.txt', 'w') as f:
+# This should make it run until it completes successfully. 
+completed = False
+while not completed:
+    hat = []
     for participant in participants:
-        f.write(participant.name + ' --> ' + participant.giving_to + '\n')
-        
-for participant in participants:
-    filename = '%s.txt' % participant.name
-    with open(filename, 'w') as file:
-        file.write(participant.giving_to)
-                     
+        hat.append(participant.name)
+    try: 
+        for participant in participants:
+            participant.giving_to = select(participant, hat)
+            hat.remove(participant.giving_to)
+        completed = True        
+    except IndexError:
+        completed = False
+
+if completed:
+    print('Success. You are awesome.')            
+    with open('full_results.txt', 'w') as f:
+        for participant in participants:
+            f.write(participant.name + ' --> ' + participant.giving_to + '\n')
+            
+    for participant in participants:
+        filename = '%s.txt' % participant.name
+        with open(filename, 'w') as file:
+            file.write(participant.giving_to)
