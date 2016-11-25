2e7f93da9b6916897e89f202f859e7efa69e3914
Joseph Marchese <joeymarchese@gmail.com>
2016-11-25 12:32:58 -0500

    Added classes. Fixed naming conventions
    still just refactoring without any major changes.

--- PolyNanna.py
+++ PolyNanna.py
@@ -10,6 +10,38 @@ class Participant:
         self.giving_to = giving_to
 
 
+class Hat:
+
+    def __init__(self, contents=None):
+        self.contents = contents
+
+
+    def build_hat(self):
+        return set([participant.name for participant in participants])
+
+
+    def select(self, person, hat):
+        okay_set = set([person for person in hat]) - person.restricted_set
+        return choice(list(okay_set))
+
+
+class Results:
+
+    def write_full_results(self):
+        with open('full_results.txt', 'w') as f:
+            for participant in participants:
+                f.write(participant.name + ' --> ' + participant.giving_to + '\n')
+
+    def write_individual_results(self):
+        if not os.path.exists(os.getcwd() + '\Individual_Results'):
+            os.mkdir(os.getcwd() + '\Individual_Results')
+        os.chdir(os.getcwd() + '\Individual_Results')
+        for participant in participants:
+            filename = '%s.txt' % participant.name
+            with open(filename, 'w') as file:
+                file.write(participant.giving_to)
+
+
 def build_participants():
     participants = []
     with open('data.txt', 'r') as data:
@@ -19,47 +51,24 @@ def build_participants():
     return participants
 
 
-def build_hat():
-    return set([participant.name for participant in participants])
-
-
-def select(person, hat):
-    okay_set = set([person for person in hat]) - person.restricted_set
-    return choice(list(okay_set))
-
-
 def run_drawing_until_completed():
     completed = False
     while not completed:
-        hat = build_hat()
+        hat = Hat()
+        hat.contents = hat.build_hat()
         try:
             for participant in participants:
-                participant.giving_to = select(participant, hat)
-                hat.remove(participant.giving_to)
+                participant.giving_to = hat.select(participant, hat.contents)
+                hat.contents.remove(participant.giving_to)
             completed = True
         except IndexError:
             completed = False
     return completed
 
 
-def write_full_results():
-    with open('full_results.txt', 'w') as f:
-        for participant in participants:
-            f.write(participant.name + ' --> ' + participant.giving_to + '\n')
-
-
-def write_individual_results():
-    if not os.path.exists(os.getcwd() + '\Individual_Results'):
-        os.mkdir(os.getcwd() + '\Individual_Results')
-    os.chdir(os.getcwd() + '\Individual_Results')
-    for participant in participants:
-        filename = '%s.txt' % participant.name
-        with open(filename, 'w') as file:
-            file.write(participant.giving_to)
-
-
 participants = build_participants()
 if run_drawing_until_completed():
     print('Success. You are awesome.')
-    write_full_results()
-    write_individual_results()
+    results = Results()
+    results.write_full_results()
+    results.write_individual_results()
