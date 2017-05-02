48f41d98e6a09c096df5b6b29708842f49688592
Joseph Marchese <joeymarchese@gmail.com>
2017-05-02 17:29:50 -0400

    passing some variables with a wildcat hack

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 from flask import Flask
+from flask import render_template
 import polynanna
+
+
 app = Flask(__name__)
 
 @app.route('/')
 def polyapp():
-    polyanna = polynanna.main()
-    return str([p.name for p in polyanna.participants])
+    return str([p.giving_to for p in polyanna.participants])
 
+@app.route('/<name>')
+def result(name=None):
+    giving_to = polyanna.get_participant_by_name(name).pop().giving_to
+    return render_template('participant.html', name=name, giving_to=giving_to)
 
 if __name__ == '__main__':
+    polyanna = polynanna.main()
     app.run()
