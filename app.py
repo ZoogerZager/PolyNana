26b63b4fd4bc9ca464cf4da4ce36033fc0312397
Joseph Marchese <joeymarchese@gmail.com>
2017-05-04 15:05:35 -0400

    add favicon.ico. modify styles. sort participants.

--- app.py
+++ app.py
@@ -6,16 +6,13 @@ import polynanna
 app = Flask(__name__)
 
 @app.route('/')
-def polyapp(participants=None):
+def polyapp():
     return render_template('index.html', polyanna=polyanna)
 
 @app.route('/<name>')
 def result(name=None):
-    if name:
-        giving_to = polyanna.get_participant_by_name(name).giving_to
-        return render_template('participant.html', name=name, giving_to=giving_to)
-    else:
-        return render_template('test.html')
+    giving_to = polyanna.get_participant_by_name(name).giving_to
+    return render_template('participant.html', name=name.title(), giving_to=giving_to)
 
 @app.route('/test/')
 def test():
