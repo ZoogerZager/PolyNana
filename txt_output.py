c40271f7ff0556bf6574040da8ca019956f17672
Joseph Marchese <joeymarchese@gmail.com>
2019-11-19 12:42:22 -0500

    update for 2019 Drawing

--- txt_output.py
+++ txt_output.py
@@ -3,11 +3,11 @@ import seed_db
 from participants import participants, history
 
 polyanna = polynanna.main()
-with open('masterlist.txt', 'w') as file:
+with open('results/masterlist.txt', 'w') as file:
 	for p in polyanna._participants:
 		file.write(p.name + ' is giving to ' + p.giving_to + '\n')
 for p in polyanna._participants:
-    filename = p.name + '.txt'
+    filename = 'results/' + p.name + '.txt'
     with open(filename, 'w') as file:
     	file.write(p.name + ' your gift recipient is ' + p.giving_to)
 
